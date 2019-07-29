a681a9bc2aaa648e68f1dfee1a90c31188c0f54c
trafficapp <hightrafficapp@gmail.com>
2019-07-29 18:36:23 -0400

    Added Squeeze Page

---
+++ js/general.js
@@ -0,0 +1,26 @@
+$(function(){
+
+$('#myemail').data('holder',$('#myemail').attr('placeholder'));
+
+$('#myemail').focusin(function(){
+    $(this).attr('placeholder','');
+});
+$('#myemail').focusout(function(){
+    $(this).attr('placeholder',$(this).data('holder'));
+});
+
+
+})
+
+function validateEmail(sEmail) {
+  var reEmail = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
+
+  if(!sEmail.match(reEmail)) {
+    alert("Invalid email address");
+    return false;
+  }
+
+  return true;
+
+}
+
